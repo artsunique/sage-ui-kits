6d3bb8f6c2cd216ee034a0e5966d536aaf1bf58e
Arts Unique <infp@artsunqie.de>
2025-03-22 14:55:03 +0100

    FC2

--- src/SageUiKitServiceProvider.php
+++ src/SageUiKitServiceProvider.php
@@ -9,10 +9,8 @@ class SageUiKitServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        // Registriert Views mit Namespace "sage-ui", z. B. sage-ui::components.text
         $this->loadViewsFrom(__DIR__ . '/../resources/views', 'sage-ui');
 
-        // Registriert PHP-Klassen (wie Text.php) im Blade-Namespace "sage-ui"
         Blade::componentNamespace('SageUiKits\\Components', 'sage-ui');
     }
 }
