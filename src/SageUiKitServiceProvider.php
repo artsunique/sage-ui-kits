d26863f9101d4aa1d19b0b2f1c4c74b81cda8264
Arts Unique <infp@artsunqie.de>
2025-03-22 16:26:41 +0100

    Date

--- src/SageUiKitServiceProvider.php
+++ src/SageUiKitServiceProvider.php
@@ -11,13 +11,11 @@ class SageUiKitServiceProvider extends ServiceProvider
     {
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'sage-ui');
 
-        Blade::component('SageUiKits\\Components\\Text', 'text');
-        Blade::component('SageUiKits\\Components\\Date', 'date');
-        Blade::component('SageUiKits\\Components\\Link', 'link');
-
-        // Optional: Make resources publishable
-        $this->publishes([
-            __DIR__.'/../resources/views' => resource_path('views/vendor/sage-ui-kits'),
-        ], 'sage-ui-kits');
+        Blade::components([
+            'text' => \SageUiKits\Components\Text::class,
+            'date' => \SageUiKits\Components\Date::class,
+            'copyright' => \SageUiKits\Components\Copyright::class,
+            'link' => \SageUiKits\Components\Link::class,
+        ]);
     }
 }
