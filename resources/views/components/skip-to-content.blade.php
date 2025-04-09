f92ce5637c0de484e7f6815227bd9cc540002dc5
andreasburget <info@artsunique.de>
2025-04-09 15:13:24 +0200

    Skip to Content

---
+++ resources/views/components/skip-to-content.blade.php
@@ -0,0 +1,8 @@
+<div class="skip-to-content">
+    <a href="#main"
+       role="link"
+       title="{!! __('Skip to content', 'sage') !!}"
+       aria-label="{!! __('Skip to content', 'sage') !!}">
+        {!! __('Skip to content', 'sage') !!}
+    </a>
+</div>
