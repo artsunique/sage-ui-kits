3c228a901a15a9e421951bb1b8a73cdfcdb9bc58
andreasburget <info@artsunique.de>
2025-04-11 10:00:56 +0200

    Heading

--- resources/views/components/article.blade.php
+++ resources/views/components/article.blade.php
@@ -1,9 +1,22 @@
+{{--
+    Verfügbare `type`-Werte für das `itemtype`-Attribut:
+    - 'Article'              → Allgemeiner Artikel
+    - 'NewsArticle'          → Nachrichtenartikel
+    - 'BlogPosting'          → Blog-Post
+    - 'ScholarlyArticle'     → Wissenschaftlicher Artikel
+    - 'TechArticle'          → Technischer Artikel
+    - 'Report'               → Bericht oder Studie
+    - 'SocialMediaPosting'   → Beitrag in sozialen Medien
+    - 'CreativeWork'         → Kreatives Werk
+--}}
+
 @props([
 'class' => 'prose prose-sm sm:prose-base dark:prose-invert',
+'type' => 'Article'
 ])
 
 <article class="{{ $class }}"
          itemscope
-         itemtype="http://schema.org/Article">
+         itemscope itemtype="https://schema.org/{{ $type }}">
     {{$slot}}
 </article>
