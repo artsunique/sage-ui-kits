89afd3af3ed3e3f85d9398709f1dc29630a7a7cc
andreasburget <info@artsunique.de>
2025-04-11 09:56:22 +0200

    Heading

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -1,3 +1,10 @@
-<main id="main" role="main" itemscope itemprop="mainContentOfPage">
+@props([
+'class' => 'prose prose-sm sm:prose-base dark:prose-invert',
+])
+
+<main id="main"
+      class="{{ $class }}"
+      role="main"
+      itemscope itemprop="mainContentOfPage">
     {{$slot}}
 </main>
